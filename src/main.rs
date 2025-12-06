d5ba07d6e95d5aee59d36a313ea9f40fa700c645
Shane Lillie <ignignokterr@gmail.com>
2025-12-06 11:09:24 -0800

    Initial import

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
+use tracing::{Level, info};
+use tracing_subscriber::FmtSubscriber;
+
+// https://dev.to/geoffreycopin/-build-a-web-server-with-rust-and-tokio-part-0-the-simplest-possible-get-handler-1lhi
+
+async fn handle_request(mut socket: TcpStream) {
+    info!("handling request");
+    let mut buf = vec![0; 1024];
+
+    loop {
+        match socket.read(&mut buf).await {
+            Ok(0) => {
+                info!("remote connection closed");
+                return;
+            }
+            Ok(n) => {
+                // TODO: replace with handling the data
+                if socket.write_all(&buf[..n]).await.is_err() {
+                    info!("unexpected socket error while writing");
+                    return;
+                }
+            }
+            Err(_) => {
+                info!("unexpected socket error while reading");
+                return;
+            }
+        }
+    }
+}
+
+fn init_logging() -> anyhow::Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)?;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    init_logging()?;
+
+    let listener = TcpListener::bind("0.0.0.0:8080").await?;
+    info!("listening at {}", listener.local_addr().unwrap());
+
+    loop {
+        let (socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            handle_request(socket).await;
+        });
+    }
+}
